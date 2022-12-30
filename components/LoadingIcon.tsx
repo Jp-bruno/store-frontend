9db45b34e356a7b1e04ed481ce4443dcf24c8cdd
João Pedro <jp.bruno.machado@gmail.com>
2022-12-30 17:31:24 -0300

    created loading component

--- components/LoadingIcon.tsx
+++ components/LoadingIcon.tsx
@@ -1,13 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Oval } from "react-loader-spinner";
 
 const StyledLoading = styled.div`
-
+  display: grid;
+  place-items: center;
 `;
 
 export default function Loading() {
-    return (
-        <StyledLoading>
-            
-        </StyledLoading>
-    )
+  return (
+    <StyledLoading>
+      <Oval
+        color="white"
+        secondaryColor="#555555"
+      />
+    </StyledLoading>
+  );
 }
